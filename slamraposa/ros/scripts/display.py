5de13ff7178f51491bdfbd8418a7ab26663f3f8a
bernardo-git <bgfccortez@gmail.com>
2019-12-07 18:38:29 +0000

    grafico das cov testado

--- slamraposa/ros/scripts/display.py
+++ slamraposa/ros/scripts/display.py
@@ -49,6 +49,14 @@ def landmarks_and_path(x,y, mean_pred, cov, plot_type, world):
     plt.grid(color='lightgray',linestyle='--')
     plt.show(block=False)
 
+
+def make_patch_spines_invisible(ax):
+    ax.set_frame_on(True)
+    ax.patch.set_visible(False)
+    for sp in ax.spines.values():
+        sp.set_visible(False)
+
+
 def cov_time(pose_cov, nr_landmarks_seen, dist):
     
     pose_cov = np.asarray(pose_cov)
@@ -56,17 +64,51 @@ def cov_time(pose_cov, nr_landmarks_seen, dist):
     dist = np.asarray(dist)
     k = np.arange(0,len(pose_cov))
 
-    plt.figure()
-    ldmkp = plt.plot(k, nr_landmarks_seen, 'p', label='nr_landmarks_seen')
-    distp = plt.plot(k, dist, 'g', label='dist')
-    covxp = plt.plot(k, pose_cov[:,0,0], 'b', label='cov_x')
-    covyp = plt.plot(k, pose_cov[:,1,1], 'r', label='cov_y')
-    covheadingp = plt.plot(k, pose_cov[:,2,2], 'aqua', label='cov_heading')
-    plt.legend(loc="upper right")
-    plt.title('Pose covariance over time')
-    plt.xlabel('k (time step)')
-    plt.ylabel('cov(k)')
-    plt.show(block=False)
+    fig, host = plt.subplots()
+    fig.subplots_adjust(right=0.75)
+
+    par1 = host.twinx()
+    par2 = host.twinx()
+
+    # Offset the right spine of par2.  The ticks and label have already been
+    # placed on the right by twinx above.
+    par2.spines["right"].set_position(("axes", 1.2))
+    # Having been created by twinx, par2 has its frame off, so the line of its
+    # detached spine is invisible.  First, activate the frame but make the patch
+    # and spines invisible.
+    make_patch_spines_invisible(par2)
+    # Second, show the right spine.
+    par2.spines["right"].set_visible(True)
+
+    p1, = host.plot(k, pose_cov[:,0,0], 'b-', label='cov_x')
+    p2, = host.plot(k, pose_cov[:,1,1], 'r-', label='cov_y')
+    p3, = host.plot(k, pose_cov[:,2,2], 'k-', label='cov_heading')
+
+    p4, = par1.plot(k, nr_landmarks_seen, 'm-', label='nr_landmarks_seen')
+    p5, = par2.plot(k, dist, 'g-', label='dist')
+
+    par1.set_ylim(0, max(nr_landmarks_seen)*2)
+
+    host.set_xlabel("Iteration, k")
+    host.set_ylabel("Covariance")
+    par1.set_ylabel("Nr of landmarks seen")
+    par2.set_ylabel("Distance")
+
+    host.yaxis.label.set_color(p3.get_color())
+    par1.yaxis.label.set_color(p4.get_color())
+    par2.yaxis.label.set_color(p5.get_color())
+
+    tkw = dict(size=4, width=1.5)
+    host.tick_params(axis='y', colors=p3.get_color(), **tkw)
+    par1.tick_params(axis='y', colors=p4.get_color(), **tkw)
+    par2.tick_params(axis='y', colors=p5.get_color(), **tkw)
+    host.tick_params(axis='x', **tkw)
+
+    lines = [p1, p2, p3]
+
+    host.legend(lines, [l.get_label() for l in lines], loc='upper left')
+
+    plt.show()
 
 def variable_individual_analysis(x_odo, y_odo, theta_odo, x, y, theta):
 
